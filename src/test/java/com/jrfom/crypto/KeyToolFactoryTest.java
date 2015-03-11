cc67578a7a953239e7866ba12798c6c11c3dd116
James Sumners <james.sumners@gmail.com>
2015-03-11 13:03:28 -0400

    Add test for KeyToolFactory

---
+++ src/test/java/com/jrfom/crypto/KeyToolFactoryTest.java
@@ -0,0 +1,34 @@
+package com.jrfom.crypto;
+
+import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Optional;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class KeyToolFactoryTest {
+
+  @Test
+  public void testGetInstance() throws Exception {
+    KeyTool keyTool = KeyToolFactory.getInstance();
+    assertNotNull(keyTool);
+
+    Optional<Key> keyOptional = keyTool.aes128key();
+    assertTrue(keyOptional.isPresent());
+  }
+
+  @Test
+  public void testGetInstanceWithRandom() throws Exception {
+    SecureRandom random = SecureRandom.getInstance("NativePRNG");
+    assertNotNull(random);
+
+    KeyTool keyTool = KeyToolFactory.getInstanceWithRandom(random);
+    assertNotNull(keyTool);
+
+    Optional<Key> keyOptional = keyTool.aes128key();
+    assertTrue(keyOptional.isPresent());
+  }
+}
